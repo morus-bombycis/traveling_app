e3a3382787c76f29c2fd58471c66194b206d3b69
morus-bombycis <christopher.perez.ubiqum@gmail.com>
2020-03-10 11:10:57 +0100

    first commit

---
+++ src/components/Landing.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Landing from './Landing';
+
+it('Landing component should render without crashing', () => {
+    shallow(<Landing />);
+});
